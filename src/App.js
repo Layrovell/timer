bb07a1066d5bb18acc35789170384ca54d25bced
Layrovell <marina95prodan@gmail.com>
2021-02-15 15:45:03 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import './styles/reset.scss';
+import { interval } from 'rxjs';
+import { Buttons } from './components/Buttons/Buttons';
+import { Countdown } from "./components/Countdown/Countdown";
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [timerOn, setTimerOn] = useState(false);
+
+  useEffect(() => {
+    const countdown$ = interval(1000)
+      .subscribe(() => {
+        if (timerOn === true) {
+          setTime(prevTime => prevTime + 1)
+        }
+      });
+
+    return () => {
+      countdown$.unsubscribe();
+    };
+  }, [timerOn]);
+
+  const startStop = () => {
+    setTimerOn(!timerOn);
+  };
+
+  const wait = () => {
+    setTimerOn(false);
+  };
+
+  const reset = () => {
+    setTime(0);
+    setTimerOn(false);
+  };
+
   return (
     <div className="App">
-      <h1 style={{fontSize: 140 + "px"}}>it works</h1>
+      <div className="box">
+        <Countdown time={time}/>
+        <Buttons
+          onStartStop={startStop}
+          onWait={wait}
+          onReset={reset}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
